828efe57379ae40f058094c6f877c431c06d8577
Dolev4 <crazydomyn@gmail.com>
2024-09-04 13:44:06 +0600

    after scripts

--- push.sh
+++ push.sh
@@ -1,3 +1,17 @@
-git add *
-git commit -m 'add new feature'
-git push
+function gitinit() {
+    git init
+    git add .
+    git commit -m "$1"
+    git branch -M main
+    git remote add origin $2
+    git push -u origin main
+}
+function gitcommit() {
+    git add .
+    git commit -m "$1"
+} 
+function gitpush() {
+    git add .
+    git commit -m "$1"
+    git push origin main
+}
